acc9b7ba8ea3a4e6bfb137bb944e8fb1911260c4
LucyC-1225 <lucyc1225xx@gmail.com>
2025-06-21 10:31:29 -0400

    recipe search

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,8 @@
-import { type Recipe, type RecipeIngredient } from "../types/type";
+import {
+  type RawRecipe,
+  type Recipe,
+  type RecipeIngredient,
+} from "../types/type";
 
 const API_URL = "http://localhost:5000/";
 
@@ -31,45 +35,63 @@ export async function getRandomRecipes(): Promise<Recipe[]> {
   return rawResults.map((mealData) => {
     const meal = mealData.meals[0];
 
-    const ingredients: RecipeIngredient[] = [];
-
-    for (let i = 1; i <= 20; i++) {
-      const ingredient = meal[`strIngredient${i}`];
-      const quantity = meal[`strMeasure${i}`];
-
-      if (ingredient && ingredient !== "") {
-        ingredients.push({
-          name: ingredient,
-          amount: quantity || "", // fallback if measure is missing
-        });
-      }
-    }
-
-    const tagsArray = meal.strTags
-      ?.split(",")
-      .map((tag: string) => tag.trim()) // remove accidental spaces
-      .filter((tag: string) => tag !== "");
-
-    const recipe: Recipe = {
-      id: meal.idMeal,
-      name: meal.strMeal,
-      area: meal.strArea,
-      category: meal.strCategory,
-      ingredients: ingredients,
-      instructions: meal.strInstructions,
-      imageUrl: meal.strMealThumb,
-      tags: tagsArray ? tagsArray : [],
-      ...(meal.strYoutube &&
-        meal.strYoutube.trim() !== "" && {
-          youtubeUrl: meal.strYoutube,
-        }),
-    };
-
-    return recipe;
+    return rawRecipeDataToRecipe(meal);
   });
+}
+
+function rawRecipeDataToRecipe(rawRecipe: RawRecipe): Recipe {
+  const ingredients: RecipeIngredient[] = [];
+
+  for (let i = 1; i <= 20; i++) {
+    // @ts-expect-error: strIngredient does go from 1-20
+    const ingredient = rawRecipe[`strIngredient${i}`];
+    // @ts-expect-error: strIngredient does go from 1-20
+    const quantity = rawRecipe[`strMeasure${i}`];
+
+    if (ingredient && ingredient !== "") {
+      ingredients.push({
+        name: ingredient,
+        amount: quantity || "", // fallback if measure is missing
+      });
+    }
+  }
+
+  const tagsArray = rawRecipe.strTags
+    ?.split(",")
+    .map((tag: string) => tag.trim()) // remove accidental spaces
+    .filter((tag: string) => tag !== "");
+
+  const recipe: Recipe = {
+    id: rawRecipe.idMeal,
+    name: rawRecipe.strMeal,
+    area: rawRecipe.strArea,
+    category: rawRecipe.strCategory,
+    ingredients: ingredients,
+    instructions: rawRecipe.strInstructions,
+    imageUrl: rawRecipe.strMealThumb,
+    tags: tagsArray ? tagsArray : [],
+    ...(rawRecipe.strYoutube &&
+      rawRecipe.strYoutube.trim() !== "" && {
+        youtubeUrl: rawRecipe.strYoutube,
+      }),
+  };
+
+  return recipe;
+}
+
+export async function getRecipesByName(name: string): Promise<Recipe[]> {
+  const res = await fetch(
+    `http://localhost:5000/api/search?q=${encodeURIComponent(name)}`
+  );
+  const data = await res.json();
+  const recipes: RawRecipe[] = data.meals;
+  return recipes?.map((recipe: RawRecipe) : Recipe => {
+    return rawRecipeDataToRecipe(recipe)
+  })?? [];
 }
 
 export default {
   getRecipe,
   getRandomRecipes,
+  getRecipesByName,
 };
--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -7,6 +7,7 @@ import RecipeCard from "../components/RecipeCard";
 export default function RecipeSearch() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [nameSearchValue, setNameSearchValue] = useState<string>("");
 
   useEffect(() => {
     initialize();
@@ -17,11 +18,21 @@ export default function RecipeSearch() {
     getRecipes();
   }
 
+  console.log(recipes);
+
   // get 6 random recipes
   async function getRecipes() {
     setIsLoading(true);
     const recipes = await api.getRandomRecipes();
-    setRecipes((prevRecipes) => [...prevRecipes, ...recipes]);
+    setRecipes((prevRecipes) => [...prevRecipes, ...(recipes || [])]);
+    setIsLoading(false);
+  }
+
+  async function handleNameSearchSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    const data = await api.getRecipesByName(nameSearchValue);
+    setRecipes(data);
     setIsLoading(false);
   }
 
@@ -31,7 +42,33 @@ export default function RecipeSearch() {
 
       <div className="flex w-full flex-1 min-h-0">
         {/* Filters section */}
-        <div className="h-full flex flex-col w-[25%] py-8 px-4 border-r-2 border-orange-800 overflow-auto">
+        <div className="h-full flex flex-col w-[25%] py-8 px-4 border-r-2 border-orange-800 overflow-auto gap-4">
+          <form
+            className="sm:flex sm:max-w-md"
+            onSubmit={(e) => handleNameSearchSubmit(e)}
+          >
+            <label htmlFor="name-search" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="name-search"
+              name="name-search"
+              type="text"
+              required
+              placeholder="Search recipe by name"
+              value={nameSearchValue}
+              onChange={(e) => setNameSearchValue(e.target.value)}
+              className="w-full min-w-0 rounded-md bg-white/5 px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-700 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-orange-700 sm:w-64 sm:text-base/6 xl:w-full"
+            />
+            <div className="mt-4 sm:mt-0 sm:ml-4 sm:shrink-0">
+              <button
+                type="submit"
+                className="flex w-full items-center justify-center rounded-md cursor-pointer bg-orange-800 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-800"
+              >
+                Search Recipe
+              </button>
+            </div>
+          </form>
           <p className="text-base font-medium">Filters</p>
         </div>
         {/* Search Section */}
@@ -41,17 +78,19 @@ export default function RecipeSearch() {
             role="list"
             className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:mx-0 lg:max-w-none lg:gap-x-8 xl:col-span-2"
           >
-            {recipes.map((recipe) => {
-              return <RecipeCard recipe={recipe} />;
-            })}
+            {recipes.length != 0
+              ? recipes.map((recipe) => {
+                  return <RecipeCard recipe={recipe} />;
+                })
+              : !isLoading && <p>No recipes found</p>}
           </div>
           {/* Load more button */}
-          <div className="w-full flex justify-center mt-3">
+          <div className="w-full flex justify-center mt-5">
             <button
               onClick={getRecipes}
-              className="cursor-pointer text-center w-24 rounded-md bg-orange-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="cursor-pointer text-center rounded-md bg-orange-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              {isLoading ? "Loading..." : "Load more"}
+              {isLoading ? "Loading..." : "Load Random Recipes"}
             </button>
           </div>
         </div>
