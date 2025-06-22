1c71c2712d34f186045358742e523a743717e12e
LucyC-1225 <lucyc1225xx@gmail.com>
2025-06-22 19:47:33 -0400

    design of category and areas filter

--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -1,8 +1,74 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import api from "../api/api";
-import type { Recipe } from "../types/type";
+import { areas, categories, type Recipe } from "../types/type";
 import RecipeCard from "../components/RecipeCard";
+import { ChevronDown } from "lucide-react";
+
+function FilterSection({ name, filters }: { name: string; filters: string[] }) {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  return (
+    <div className="flex flex-col">
+      <div
+        className="flex justify-between cursor-pointer"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <p className="font-semibold">Filter by {name}</p>
+        <ChevronDown className="w-5 h-5" />
+      </div>
+
+      {isOpen && (
+        <div className="grid grid-cols-2 mt-2">
+          {filters.map((filter) => {
+            return (
+              <div>
+                <div className="flex gap-3">
+                  <div className="flex h-6 shrink-0 items-center">
+                    <div className="group grid size-4 grid-cols-1">
+                      <input
+                        id="comments"
+                        name="comments"
+                        type="checkbox"
+                        aria-describedby="comments-description"
+                        className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-orange-700 checked:bg-orange-700 indeterminate:border-orange-700 indeterminate:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-700 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                      />
+                      <svg
+                        fill="none"
+                        viewBox="0 0 14 14"
+                        className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                      >
+                        <path
+                          d="M3 8L6 11L11 3.5"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="opacity-0 group-has-checked:opacity-100"
+                        />
+                        <path
+                          d="M3 7H11"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="opacity-0 group-has-indeterminate:opacity-100"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="text-sm/6">
+                    <label htmlFor="comments" className="text-gray-900">
+                      {filter}
+                    </label>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function RecipeSearch() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -42,7 +108,7 @@ export default function RecipeSearch() {
 
       <div className="flex w-full flex-1 min-h-0">
         {/* Filters section */}
-        <div className="h-full flex flex-col w-[25%] py-8 px-4 border-r-2 border-orange-800 overflow-auto gap-4">
+        <div className="h-full flex flex-col w-[25%] pt-8 px-4 border-r-2 border-orange-800 overflow-auto gap-4 scrollbar-hide">
           <form
             className="sm:flex sm:max-w-md"
             onSubmit={(e) => handleNameSearchSubmit(e)}
@@ -69,7 +135,20 @@ export default function RecipeSearch() {
               </button>
             </div>
           </form>
-          <p className="text-base font-medium">Filters</p>
+
+          {/* Filter by category */}
+          <FilterSection name="category" filters={[...categories]} />
+
+          {/* Filter by area */}
+          <FilterSection name="area" filters={[...areas]} />
+
+          <div className="w-full flex justify-center">
+            <button
+              className="cursor-pointer text-center rounded-md bg-orange-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-700"
+            >
+              Apply Filters
+            </button>
+          </div>
         </div>
         {/* Search Section */}
         <div className="w-[75%] flex flex-col px-12 py-8 overflow-auto">
@@ -88,7 +167,7 @@ export default function RecipeSearch() {
           <div className="w-full flex justify-center mt-5">
             <button
               onClick={getRecipes}
-              className="cursor-pointer text-center rounded-md bg-orange-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="cursor-pointer text-center rounded-md bg-orange-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-700"
             >
               {isLoading ? "Loading..." : "Load Random Recipes"}
             </button>
