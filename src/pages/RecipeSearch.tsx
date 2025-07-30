0cc7022a295566354144e53f64f563b9a39d66a6
LucyC-1225 <lucyc1225xx@gmail.com>
2025-07-29 21:43:56 -0400

    recipe card new design

--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -228,7 +228,7 @@ export default function RecipeSearch() {
 
             {/* Results Grid */}
             {recipes.length > 0 ? (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 items-start">
                 {recipes.map((recipe) => (
                   <RecipeCard key={recipe.id} recipe={recipe} userRecipes={userRecipes} />
                 ))}
